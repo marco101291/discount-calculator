ef7442d3c7fafd0a038b847afee94c9d97f00f54
Marco Antonio Lievano Escobar <marco.lievano@digitalonus.com>
2023-04-02 14:02:58 -0600

    deleted unused code

--- App.js
+++ App.js
@@ -40,14 +40,10 @@ export default function App() {
     setDiscount(null)
   }
 
-  // const resultStyle = { color: typeof (result) === 'number' ? 'green' : typeof (result) === 'string' ? 'red' : 'black' };
+ 
   const backgrColors = ['#4158D0', '#C850C0', '#FFCC70']
   const btnColors = ['#77A1D3', '#79CBCA', '#77A1D3']
 
-
-  // useEffect(() => {
-
-  // }, [result, amount, discount])
 
   return (
     <SafeAreaView style={styles.container}>
@@ -84,34 +80,3 @@ export default function App() {
     </SafeAreaView>
   );
 }
-
-
-// <SafeAreaView style={styles.container}>
-//   <View style={styles.avatar_container} >
-//     <View style={{
-//       justifyContent: 'center',
-//       alignItems: 'center',
-//     }}>
-//       <Image style={{ marginBottom: 10 }} source={require('./assets/batman-avatar.png')} />
-//       <Text style={{ maxWidth: 200 }}>"It's not who I am underneath, but what I do that that defines me"</Text>
-//     </View>
-//   </View>
-//   <View style={styles.details_container}>
-//     <View style={styles.item}>
-//       <Text style={styles.label}>Name: </Text>
-//       <Text style={styles.data}>Batman </Text>
-//     </View>
-//     <View style={styles.item}>
-//       <Text style={styles.label}>City: </Text>
-//       <Text style={styles.data}>Gotham </Text>
-//     </View>
-//     <View style={styles.item}>
-//       <Text style={styles.label}>Fav Color: </Text>
-//       <Text style={styles.data}>Black </Text>
-//     </View>
-//     <View style={styles.item}>
-//       <Text style={styles.label}>Job: </Text>
-//       <Text style={styles.data}>Battling </Text>
-//     </View>
-//   </View >
-// </SafeAreaView>
